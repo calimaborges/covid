577ad6438f248b253f830afb208c9449557e55a3
Carlos Augusto Borges <calimaborges@gmail.com>
2020-04-09 06:33:52 -0300

    Mensagens de status

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import parseCsv from "./parseCsv";
 import getCsvUrl from "./getCsvUrl";
 
 async function main() {
+  const mainComponent = document.getElementById("root")
+  mainComponent.innerHTML = `<p>Obtendo endereço do CSV...</p>`;
   const url = await getCsvUrl();
+
+  mainComponent.innerHTML = `<p>Obtendo CSV de ${url}...</p>`;
   const csv = await parseCsv(url);
 
   ReactDOM.render(
     <React.StrictMode>
       <App data={csv.data} />
     </React.StrictMode>,
-    document.getElementById("root")
+    mainComponent
   );
 }
 
