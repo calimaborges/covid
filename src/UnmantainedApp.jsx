da35c0690cab20472c2cb34a18587cfec2098d56
Carlos Augusto Borges <calimaborges@gmail.com>
2020-06-06 21:16:03 -0300

    Adiciona informações de arquivos de dados

--- src/UnmantainedApp.jsx
+++ src/UnmantainedApp.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./App.module.css";
 
-function UnmantainedApp() {
+function UnmantainedApp({ dadosArquivo }) {
   return (
     <div className={styles.Container}>
       <h1>Site fora do ar</h1>
@@ -10,16 +10,15 @@ function UnmantainedApp() {
         fornecido pelo Ministério da Saúde.
       </p>
       <p>
-        Não se preocupe! Os dados, na realidade, estão até mais completos porém
-        não tive tempo para adaptar a página. Além disso o próprio Ministério da
-        Saúde atualizou a página deles com novos gráficos. Acesse{" "}
-        <a href="https://covid.saude.gov.br/">https://covid.saude.gov.br/</a>.
-      </p>
-      <p>
         O código fonte do sistema antigo ainda está disponível no GitHub. Acesse{" "}
         <a href="https://github.com/calimaborges/covid.git">
           https://github.com/calimaborges/covid.git
         </a>
+      </p>
+      <p>
+        <a href={dadosArquivo.url}>Clique aqui para baixar o arquivo de dados</a>
+        <br />
+        Atualizado em {dadosArquivo.dataAtualizacao}
       </p>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./UnmantainedApp";
 import ErrorBoundary from "./ErrorBoundary";
+import getCsvInfo from "./getCsvInfo";
 
 async function main() {
+  const dadosArquivo = await getCsvInfo();
+
   ReactDOM.render(
     <React.StrictMode>
       <ErrorBoundary>
-        <App />
+        <App dadosArquivo={dadosArquivo} />
       </ErrorBoundary>
     </React.StrictMode>,
     document.getElementById("root")
